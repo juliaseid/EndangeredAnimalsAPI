a3a2e7e206d44405a8072ff655632d88bf8e2681
Julia Seidman <juliaseid@gmail.com>
2020-06-16 11:27:47 -0700

    more animals

--- Migrations/20200616182713_MoreAnimals.Designer.cs
+++ Migrations/20200616182713_MoreAnimals.Designer.cs
@@ -2,14 +2,16 @@
 using EndangeredAnimalsAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EndangeredAnimalsAPI.Migrations
 {
     [DbContext(typeof(EndangeredAnimalsAPIContext))]
-    partial class EndangeredAnimalsAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20200616182713_MoreAnimals")]
+    partial class MoreAnimals
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
