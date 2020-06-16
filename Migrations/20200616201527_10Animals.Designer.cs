7cafee2b289b1b1d3231ed34fb10a197ee576ede
Julia Seidman <juliaseid@gmail.com>
2020-06-16 13:15:55 -0700

    seed database with 10 total animals

--- Migrations/20200616201527_10Animals.Designer.cs
+++ Migrations/20200616201527_10Animals.Designer.cs
@@ -2,14 +2,16 @@
 using EndangeredAnimalsAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EndangeredAnimalsAPI.Migrations
 {
     [DbContext(typeof(EndangeredAnimalsAPIContext))]
-    partial class EndangeredAnimalsAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20200616201527_10Animals")]
+    partial class _10Animals
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
